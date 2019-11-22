ad4896dc59c0cf60e8ad246d0e9eb9719fa79140
Lucas <lucaslelaidier@gmail.com>
2019-11-22 12:45:59 +0100

    controller livre done

---
+++ src/controller/LivreController.java
@@ -0,0 +1,87 @@
+package controller;
+
+import model.Livre;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class LivreController {
+	DatabaseController db;
+
+	public LivreController(DatabaseController db) {
+		this.db = db;
+	}
+
+	public ArrayList<Livre> getAllLivres() throws SQLException {
+		ResultSet livres = db.query("select * from Livres order by titre asc;");
+		ArrayList<Livre> result = new ArrayList<>();
+
+		while (livres.next()) {
+			Livre livre = new Livre();
+
+			livre.setId(livres.getInt("LIV_ID"));
+			livre.setTitre(livres.getString("LIV_TITRE"));
+			livre.setAuteur(livres.getString("LIV_AUTEUR"));
+			livre.setDate(livres.getDate("LIV_DATE"));
+
+			result.add(livre);
+		}
+
+		return result;
+	}
+
+	public ArrayList<Livre> getLivreByTitle(String titre) throws SQLException {
+		ResultSet livres = db.query("select * from Livres where LIV_TITRE = '" + titre + "' order by titre asc;");
+		ArrayList<Livre> result = new ArrayList<>();
+
+		while (livres.next()) {
+			Livre livre = new Livre();
+
+			livre.setId(livres.getInt("LIV_ID"));
+			livre.setTitre(livres.getString("LIV_TITRE"));
+			livre.setAuteur(livres.getString("LIV_AUTEUR"));
+			livre.setDate(livres.getDate("LIV_DATE"));
+
+			result.add(livre);
+		}
+
+		return result;
+	}
+
+	public ArrayList<Livre> getLivreByAuthor(String author) throws SQLException {
+		ResultSet livres = db.query("select * from Livres where LIV_AUTEUR = '" + author + "' order by titre asc;");
+		ArrayList<Livre> result = new ArrayList<>();
+
+		while (livres.next()) {
+			Livre livre = new Livre();
+
+			livre.setId(livres.getInt("LIV_ID"));
+			livre.setTitre(livres.getString("LIV_TITRE"));
+			livre.setAuteur(livres.getString("LIV_AUTEUR"));
+			livre.setDate(livres.getDate("LIV_DATE"));
+
+			result.add(livre);
+		}
+
+		return result;
+	}
+
+	public ArrayList<Livre> getLivreByDate(String date) throws SQLException {
+		ResultSet livres = db.query("select * from Livres where LIV_DATE = '" + date + "' order by titre asc;");
+		ArrayList<Livre> result = new ArrayList<>();
+
+		while (livres.next()) {
+			Livre livre = new Livre();
+
+			livre.setId(livres.getInt("LIV_ID"));
+			livre.setTitre(livres.getString("LIV_TITRE"));
+			livre.setAuteur(livres.getString("LIV_AUTEUR"));
+			livre.setDate(livres.getDate("LIV_DATE"));
+
+			result.add(livre);
+		}
+
+		return result;
+	}
+}
