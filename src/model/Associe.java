85754aeeb077aba163f90958bb25b46313e8dc2f
Lucas <lucaslelaidier@gmail.com>
2019-11-22 13:17:57 +0100

    r

--- src/model/Associe.java
+++ src/model/Associe.java
@@ -19,23 +19,4 @@ public class Associe {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
 }
