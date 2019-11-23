edf3c73d0449dee78bfe86c2796cb7d94cf1fb15
Lucas Lelaidier <lucaslelaidier@gmail.com>
2019-11-23 13:56:31 +0100

    refactored code to avoid duplication

--- src/controller/AssocieController.java
+++ src/controller/AssocieController.java
@@ -17,55 +17,28 @@ public class AssocieController {
 		ResultSet associes = db.query("select * from Associe where ASS_NOM = '" + name + "' AND ASS_PRENOM = '" + lastname + "';");
 		ArrayList<Associe> result = new ArrayList<>();
 
-		while(associes.next()) {
-			Associe associe = new Associe();
-
-			associe.setId(associes.getInt("ASS_ID"));
-			associe.setNom(associes.getString("ASS_NOM"));
-			associe.setPrenom(associes.getString("ASS_PRENOM"));
-
-			result.add(associe);
-		}
-
-		return result;
+		return populateList(associes);
 	}
 
 	public ArrayList<Associe> getAssocieByName(String name) throws SQLException {
 		ResultSet associes = db.query("select * from Associe where ASS_NOM = '" + name + "';");
-		ArrayList<Associe> result = new ArrayList<>();
 
-		while(associes.next()) {
-			Associe associe = new Associe();
-
-			associe.setId(associes.getInt("ASS_ID"));
-			associe.setNom(associes.getString("ASS_NOM"));
-			associe.setPrenom(associes.getString("ASS_PRENOM"));
-
-			result.add(associe);
-		}
-
-		return result;
+		return populateList(associes);
 	}
 
 	public ArrayList<Associe> getAssocieByLastname(String lastname) throws SQLException {
 		ResultSet associes = db.query("select * from Associe where ASS_PRENOM = '" + lastname + "';");
-		ArrayList<Associe> result = new ArrayList<>();
 
-		while(associes.next()) {
-			Associe associe = new Associe();
-
-			associe.setId(associes.getInt("ASS_ID"));
-			associe.setNom(associes.getString("ASS_NOM"));
-			associe.setPrenom(associes.getString("ASS_PRENOM"));
-
-			result.add(associe);
-		}
-
-		return result;
+		return populateList(associes);
 	}
 
 	public ArrayList<Associe> getAssocieWhoBorrowedLivre(String titre) throws SQLException {
 		ResultSet associes = db.query("select * from Emprunte join Livre using(LIV_ID) join Associe using(ASS_ID) where LIV_TITRE = '" + titre + "';");
+
+		return populateList(associes);
+	}
+
+	private ArrayList<Associe> populateList(ResultSet associes) throws SQLException {
 		ArrayList<Associe> result = new ArrayList<>();
 
 		while(associes.next()) {
--- src/controller/LivreController.java
+++ src/controller/LivreController.java
@@ -15,78 +15,35 @@ public class LivreController {
 
 	public ArrayList<Livre> getAllLivres() throws SQLException {
 		ResultSet livres = db.query("select * from Livres order by titre asc;");
-		ArrayList<Livre> result = new ArrayList<>();
 
-		while (livres.next()) {
-			Livre livre = new Livre();
-
-			livre.setId(livres.getInt("LIV_ID"));
-			livre.setTitre(livres.getString("LIV_TITRE"));
-			livre.setAuteur(livres.getString("LIV_AUTEUR"));
-			livre.setDate(livres.getDate("LIV_DATE"));
-
-			result.add(livre);
-		}
-
-		return result;
+		return populateList(livres);
 	}
 
 	public ArrayList<Livre> getLivreByTitle(String titre) throws SQLException {
 		ResultSet livres = db.query("select * from Livres where LIV_TITRE = '" + titre + "' order by titre asc;");
-		ArrayList<Livre> result = new ArrayList<>();
 
-		while (livres.next()) {
-			Livre livre = new Livre();
-
-			livre.setId(livres.getInt("LIV_ID"));
-			livre.setTitre(livres.getString("LIV_TITRE"));
-			livre.setAuteur(livres.getString("LIV_AUTEUR"));
-			livre.setDate(livres.getDate("LIV_DATE"));
-
-			result.add(livre);
-		}
-
-		return result;
+		return populateList(livres);
 	}
 
 	public ArrayList<Livre> getLivreByAuthor(String author) throws SQLException {
 		ResultSet livres = db.query("select * from Livres where LIV_AUTEUR = '" + author + "' order by titre asc;");
-		ArrayList<Livre> result = new ArrayList<>();
 
-		while (livres.next()) {
-			Livre livre = new Livre();
-
-			livre.setId(livres.getInt("LIV_ID"));
-			livre.setTitre(livres.getString("LIV_TITRE"));
-			livre.setAuteur(livres.getString("LIV_AUTEUR"));
-			livre.setDate(livres.getDate("LIV_DATE"));
-
-			result.add(livre);
-		}
-
-		return result;
+		return populateList(livres);
 	}
 
 	public ArrayList<Livre> getLivreByDate(String date) throws SQLException {
 		ResultSet livres = db.query("select * from Livres where LIV_DATE = '" + date + "' order by titre asc;");
-		ArrayList<Livre> result = new ArrayList<>();
 
-		while (livres.next()) {
-			Livre livre = new Livre();
-
-			livre.setId(livres.getInt("LIV_ID"));
-			livre.setTitre(livres.getString("LIV_TITRE"));
-			livre.setAuteur(livres.getString("LIV_AUTEUR"));
-			livre.setDate(livres.getDate("LIV_DATE"));
-
-			result.add(livre);
-		}
-
-		return result;
+		return populateList(livres);
 	}
 
 	public ArrayList<Livre> getBurrowedLivres(String name, String lastname) throws SQLException {
 		ResultSet livres = db.query("select * from Emprunte join Livre using(LIV_ID) join Associe using(ASS_ID) where ASS_NOM = '" + name + "' AND ASS_PRENOM = '" + lastname + "' order by titre asc;");
+
+        return populateList(livres);
+	}
+
+	private ArrayList<Livre> populateList(ResultSet livres) throws SQLException {
 		ArrayList<Livre> result = new ArrayList<>();
 
 		while (livres.next()) {
